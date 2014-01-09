6347c7aef1beda01f409a6b2351e70400508abff
stephband <stephband@googlemail.com>
2014-01-09 13:53:19 +0100

    Adds tests to event.note mixin in debug mode.

--- js/scribe.mixin.note.js
+++ js/scribe.mixin.note.js
@@ -19,28 +19,70 @@
 		};
 	}
 	
-	function testDegree(n) {
-		if (n < 0 || n > 11) {
-			throw new Error(n + 'is out of range [0-11].');
+	function testInteger(n) {
+		if (typeof n % 1 !== 0) {
+			throw new Error(n + 'is not an integer.')
+		}
+	}
+	
+	function testNumber(n) {
+		if (typeof n !== 'number') {
+			throw new Error(n + 'is not a number.')
+		}
+	}
+	
+	function testPositive() {
+		if (n < 0) {
+			throw new Error(n + 'is negative.');
+		}
+	}
+	
+	function testRange(n, min, max) {
+		if (n < min || n > max) {
+			throw new Error(n + 'is out of range [' + min + '-' + max + '].');
 		}
 	}
 	
 	Scribe.mixin.note = {
 		number: createProperty('number',
-			function(n) { this[3] = n; },
+			function(n) {
+				if (debug) {
+					testNumber(n);
+					testInteger(n);
+					testPositive(n);
+				}
+				
+				this[3] = n;
+			},
+			
 			function()  { return this[3]; }
 		),
 		
 		octave: createProperty('octave',
-			function(n) { this.number = n * 12 + this.degree; },
+			function(n) {
+				if (debug) {
+					testNumber(n);
+					testInteger(n);
+					testRange(n, 0, 16);
+				}
+				
+				this.number = n * 12 + this.degree;
+			},
+			
 			function()  { return Math.floor(this.number / 12); }
 		),
 		
 		degree: createProperty('degree',
 			function(n) {
-				testDegree(n);
+				if (debug) {
+					testNumber(n);
+					testInteger(n);
+					testRange(n, 0, 11);
+				}
+				
 				this.number = this.octave + n;
 			},
+			
 			function()  { return this.number % 12; }
 		)
 	};
