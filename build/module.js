5a2e6be48b95ba12e52171e3d4561071eba65308
Stephen <stephband@cruncher.ch>
2025-10-16 16:19:35 +0200

    Build

--- build/module.js
+++ build/module.js
@@ -2,5 +2,5 @@
    0.4.5
    By Stephen */
 
-import"./modules/chunk-D4O2MLNM.js";var r=class t{constructor(o){}static from(o){return new t(o)}};export{r as default};
+import"./modules/chunk-NYDLUXFV.js";var r=class t{constructor(o){}static from(o){return new t(o)}};export{r as default};
 //# sourceMappingURL=module.js.map
